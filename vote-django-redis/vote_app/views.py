74e4d229e0f3d3688c90bb871f0855914f948afb
xxl4tomxu98 <tomxu@udel.edu>
2023-11-11 22:42:30 -0500

    created django redis store folder

--- vote-django-redis/vote_app/views.py
+++ vote-django-redis/vote_app/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from redis import Redis
 from django.conf import settings
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.views.decorators.cache import cache_page
 from django.core.cache import cache
 import os
+import json
 import socket
 import random
-import json
-import logging
+
 
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
@@ -20,21 +19,20 @@ CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 @cache_page(CACHE_TTL)
 def home(request):
-    print(request.build_absolute_uri()) #optional
+    
+    data = {}
+    
     voter_id = request.COOKIES.get('voter_id')
-
     if not voter_id:
-        voter_id = hex(random.getrandbits(64))[2:-1]
+        voter_id = hex(random.getrandbits(64))[2:-1] 
 
     vote = None
     if request.method == 'POST':
-        redis = get_redis()
-        vote = request.form['vote']
-        app.logger.info('Received vote for %s', vote)
+        vote = request.POST.get('vote')
         data = json.dumps({'voter_id': voter_id, 'vote': vote})
-        redis.rpush('votes', data)
-        
-    return render(
+        cache.set('votes', data)
+
+    resp = render(
         request,
         'vote_app/index.html',
         {
@@ -44,3 +42,6 @@ def home(request):
             "vote": vote,
         }
     )
+
+    resp.set_cookie('voter_id', voter_id)
+    return resp
